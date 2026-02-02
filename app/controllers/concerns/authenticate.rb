0ff64936ebedc045426850df65498a2a5ac64ae9
Shawn Kearney <shawnpk29@me.com>
2026-02-02 14:07:16 -0700

    Add ability to log out

--- app/controllers/concerns/authenticate.rb
+++ app/controllers/concerns/authenticate.rb
@@ -25,6 +25,10 @@ module Authenticate
     cookies.encrypted.permanent[:app_session] = { value: app_session.to_h }
   end
 
+  def log_out
+    Current.app_session&.destroy
+  end
+
   def logged_in?
     Current.user.present?
   end
